0592ae863a6b0b0b09c7766e2796aec83f87b36f
AvatarZorak <alexhristov2005@gmail.com>
2023-03-10 04:05:02 +0200

    sending emails with images

--- send_emails.py
+++ send_emails.py
@@ -1,6 +1,7 @@
 from email.message import EmailMessage
 import ssl
 import smtplib
+import imghdr
 
 email_sender = 'hacktues9assap@gmail.com'
 email_password = 'okgexyrallzbuhcp'
@@ -16,6 +17,13 @@ em['From'] = email_sender
 em['To'] = email_receiver
 em['Subject'] = subject
 em.set_content(body)
+
+with open("image.jpg", "rb") as image:
+    file_data = image.read()
+    file_type = imghdr.what(image.name)
+    file_name = image.name
+
+em.add_attachment(file_data, maintype = "image", subtype = file_type, filename = file_name)
 
 context = ssl.create_default_context()
 
